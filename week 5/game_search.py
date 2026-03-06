96ee1728f00e8a40d044722e480fcd3003bfa186
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-03-06 15:48:45 +0530

    Update game_search.py

--- week 5/game_search.py
+++ week 5/game_search.py
@@ -1,12 +1,9 @@
 board = [' '] * 9
-
 def win(p):
     w = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
     return any(board[a]==board[b]==board[c]==p for a,b,c in w)
-
 def full():
     return ' ' not in board
-
 def minimax(ai):
     if win('O'): return 1
     if win('X'): return -1
@@ -44,3 +41,4 @@ print(board[0:3],"\n",board[3:6],"\n",board[6:9])
 if win('X'): print("You win")
 elif win('O'): print("AI wins")
 else: print("Draw")
+
